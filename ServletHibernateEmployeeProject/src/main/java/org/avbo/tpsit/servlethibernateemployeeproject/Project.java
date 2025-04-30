7b1461220ea2103c9a5d40044335a039a2b3d5e6
andre <andrea.ianni@avbo.it>
2025-04-30 16:11:07 +0200

    Definita la classe Project, facendo in modo che mappi la tabella avente lo stesso nome, abbia due attributi con i relativi riferimenti alle rispettive colonne della tabella e una lista di Employee ottenuta tramite una relazione ManyToMany, che però è stata definita nell'attributo projects della classe Employee
    Aggiunti anche i relativi Get, Set

--- ServletHibernateEmployeeProject/src/main/java/org/avbo/tpsit/servlethibernateemployeeproject/Project.java
+++ ServletHibernateEmployeeProject/src/main/java/org/avbo/tpsit/servlethibernateemployeeproject/Project.java
@@ -4,10 +4,58 @@
  */
 package org.avbo.tpsit.servlethibernateemployeeproject;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
- * @author andre
+ * @author Andrea Iannì
  */
+@Entity
+@Table(name = "Project")
 public class Project {
-    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private Integer id;
+    @Column(name = "title")
+    private String title;
+    //Il fetch EAGER indica che la lista deve essere popolata sin da subito,
+    //	a differenza di LAZY che permette di fare la query per popolarla solo
+    //	quando i dati vengono chiesti
+    @ManyToMany(mappedBy = "projects", fetch = FetchType.EAGER)
+    private Set<Employee> employees = new HashSet<>();
+
+    public Integer getId() {
+	return id;
+    }
+
+    public void setId(Integer id) {
+	this.id = id;
+    }
+
+    public String getTitle() {
+	return title;
+    }
+
+    public void setTitle(String title) {
+	this.title = title;
+    }
+
+    public Set<Employee> getEmployees() {
+	return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+	this.employees = employees;
+    }
+
 }
